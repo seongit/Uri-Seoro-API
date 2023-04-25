c635d9fc9e189dbd4d4014a804d74bf414564e11
김성은 <seongeunk102@gmail.com>
2023-04-25 18:19:18 +0900

    fix : class명 변경 (sonarController -> SonarController)
    1. SonarController로 변경

--- src/main/java/com/sekim/uriseoroapi/uriseoroapi/controller/sonarController.java
+++ src/main/java/com/sekim/uriseoroapi/uriseoroapi/controller/sonarController.java
@@ -1,22 +1,16 @@
 package com.sekim.uriseoroapi.uriseoroapi.controller;
 
-import com.sekim.uriseoroapi.uriseoroapi.config.enums.AdminAuth;
-import com.sekim.uriseoroapi.uriseoroapi.config.enums.BaseURLType;
 import com.sekim.uriseoroapi.uriseoroapi.service.SonarApiService;
 import lombok.RequiredArgsConstructor;
 import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.reactive.function.client.ExchangeStrategies;
-import org.springframework.web.reactive.function.client.WebClient;
 
 @RestController
 @CrossOrigin("*")
 @RequiredArgsConstructor
 @RequestMapping("/sonar")
-public class sonarController {
+public class SonarController {
 
     @Autowired
     private final SonarApiService sonarApiService;
